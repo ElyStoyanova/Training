18b825339aa7a0a26aa33de2d436574811aa4c35
ElyStoyanova <elen27@abv.bg>
2022-08-19 17:49:54 +0300

    change commit

--- Exerciise 1/Program.cs
+++ Exerciise 1/Program.cs
@@ -13,10 +13,12 @@ namespace Exerciise_1
             groups["Beatles"].Add("Jonh");
             groups["Beatles"].Add("Poul");
             groups["Beatles"].Add("Ringo");
+            groups["Beatles"].Add("Pesho");
 
             groups.Add("Signal", new List<string>());
             groups["Signal"].Add("Ivan");
             groups["Signal"].Add("Todor");
+            groups["Signal"].Add("Viktor");
 
             var newGroups = new Dictionary<string, List<string>>();
             newGroups = groups.Take(1).ToDictionary(x => x.Key, x => x.Value);
