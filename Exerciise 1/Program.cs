fbbf93146ee01b0f1be1ef864de3b06f2e3f713a
ElyStoyanova <elen27@abv.bg>
2022-08-19 17:45:30 +0300

    first commit

---
+++ Exerciise 1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exerciise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var groups = new Dictionary<string, List<string>>();
+            groups.Add("Beatles",new List<string>());
+            groups["Beatles"].Add("Jonh");
+            groups["Beatles"].Add("Poul");
+            groups["Beatles"].Add("Ringo");
+
+            groups.Add("Signal", new List<string>());
+            groups["Signal"].Add("Ivan");
+            groups["Signal"].Add("Todor");
+
+            var newGroups = new Dictionary<string, List<string>>();
+            newGroups = groups.Take(1).ToDictionary(x => x.Key, x => x.Value);
+
+            foreach (var item in newGroups)
+            {
+                Console.WriteLine($"{item.Key}");
+                Console.Write("-- ");
+                Console.WriteLine(string.Join("\n-- ", item.Value));
+            }
+                
+            
+            
+        }
+    }
+}
